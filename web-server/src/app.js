d68d5d840b06e027b2e9df09ac2856c7bb955805
jjokela <jarmo.jokela@gmail.com>
2022-08-26 22:04:08 +0200

    weather app continues + css

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 // ES needs this
 import path from 'path';
 import { fileURLToPath } from 'url';
+import hbs from 'hbs'
 
 // works only in require()
 //console.log(__dirname)
@@ -17,19 +18,52 @@ console.log(__filename)
 const pathToPublic = path.join(__dirname, '../public')
 console.log(`Path to public: ${pathToPublic}`)
 
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
 const app = express()
 const port = 3000
 
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
 app.use(express.static('public'))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Isi-3000'
+    })
+})
+
 app.get('/weather', (req, res) => {
-    
     const result = {
         location: 'loc',
         forecast: 'fc'
     }
-    
     res.send(result)
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Isi-3000'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'Need elp? Got yelp!',
+        name: 'Isi-3000'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Page not found :('
+    })
 })
 
 app.listen(port, () => {
