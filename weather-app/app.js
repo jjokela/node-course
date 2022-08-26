ff7f7c2ed1c20ebb5df5cb7ef40d63771a058c14
CARDANOADS\j.jokela <j.jokela@cardano.com>
2022-08-26 11:00:08 +0200

    finish weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,32 +5,49 @@ dotenv.config()
 
 // console.log(process.env)
 
-const weatherStackUrl = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=52.084176217705576,4.283449698574463`
+function getWeather(coordinates) {
 
-axios
-    .get(weatherStackUrl, {responseType: 'json'})
-    .then(res => {
-        // console.log(`statusCode: ${res.status}`)
-        //console.log(res)
-        // console.log(res.data)
-        // console.log(`current JSON string: ${JSON.stringify(res.data.current)}`)
+    const weatherStackUrl = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=${coordinates}`
 
-        let current = res.data.current
-        console.log(`Weather now is ${current.weather_descriptions.join()}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
-    })
-    .catch(error => {
-        console.log(`Error! ${error}`)
-    })
+    axios
+        .get(weatherStackUrl, { responseType: 'json' })
+        .then(res => {
+            // console.log(`statusCode: ${res.status}`)
+            //console.log(res)
+            // console.log(res.data)
+            // console.log(`current JSON string: ${JSON.stringify(res.data.current)}`)
+            getWeatherSuccess(res)
+        })
+        .catch(error => {
+            console.log(`Error! ${error}`)
+        })
+}
 
 
-const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_KEY}`
+function geocode(location) {
 
-axios
-    .get(mapBoxUrl, {responseType: 'json'})
-    .then(res => {
-        let features = res.data.features[0]        
-        console.log(`Place: ${features.place_name}, Coordinates: ${features.center.join()}`)
-    })
-    .catch(error => {
-        console.log(`Error! ${error}`)
-    })
+    const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(location)}.json?access_token=${process.env.MAPBOX_KEY}`
+
+    axios
+        .get(mapBoxUrl, { responseType: 'json' })
+        .then(res => {
+            geocodeSuccess(res)
+            getWeather(res.data.features[0].center.join())
+        })
+        .catch(error => {
+            console.log(`Error! ${error}`)
+        })
+}
+
+
+function geocodeSuccess(res) {
+    let features = res.data.features[0]
+    console.log(`Place: ${features.place_name}, Coordinates: ${features.center.join()}`)
+}
+
+function getWeatherSuccess(res) {
+    let current = res.data.current
+    console.log(`Weather now is ${current.weather_descriptions.join()}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
+}
+
+geocode('The Hague')
