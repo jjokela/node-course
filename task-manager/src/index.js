e8371616071ebfc5a7e5b00856d191783fbc24b5
jjokela <jarmo.jokela@gmail.com>
2022-08-29 21:59:50 +0200

    delete tasks and users

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -12,21 +12,21 @@ app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (err) {
-        res.status(400).send(err)
-    }
-})
-
 app.get('/users', async (req, res) => {
 
     try {
         const users = await User.find({})
         res.send(users)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+app.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send(user)
     } catch (err) {
         res.status(400).send(err)
     }
@@ -46,11 +46,57 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }) // new means return back the modified object
+        if (user) {
+            res.send(user)
+        }
+        res.status(404).send()
+
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (user) {
+            return res.send(user)
+        }
+        req.status(404).send()
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
 app.get('/tasks', async (req, res) => {
 
     try {
         const tasks = await Task.find({})
         res.send(tasks)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
     } catch (err) {
         res.status(400).send(err)
     }
@@ -71,12 +117,39 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+app.patch('/tasks/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
     try {
-        await task.save()
-        res.status(201).send(task)
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }) // new means return back the modified object
+        if (task) {
+            res.send(task)
+        }
+        res.status(404).send()
+
     } catch (err) {
         res.status(400).send(err)
     }
 })
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (task) {
+            return res.send(task)
+        }
+        req.status(404).send()
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
